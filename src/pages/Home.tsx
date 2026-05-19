90959e002c0ecdc8b912923de68f21f7c826f153
tyeyty <51113753+tyeyty@users.noreply.github.com>
2026-05-19 08:35:55 -0500

    Update blog link to point to '/board/blog'

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -177,7 +177,7 @@ const education = [
             View My Works
           </a>
           <a
-            href="/blog"
+            href="/board/blog"
             className="px-5 sm:px-6 py-2.5 sm:py-3 rounded-lg border border-gray-300 text-sm font-semibold text-gray-700 hover:bg-gray-50 transition"
           >
             Dev Blog
